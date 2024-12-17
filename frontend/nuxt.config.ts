437ec168124a6c84dfc06a64b2460fc8453e31b4
yongwoon kim <yonyon.japan@gmail.com>
2024-12-17 16:15:13 +0900

    Setup Vuetify

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,4 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  vite: {
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5002',
+          changeOrigin: true,
+        },
+      },
+    },
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
+  css: ['vuetify/styles'],
+  build: {
+    transpile: ['vuetify'],
+  },
 });
---
+++ frontend/plugins/vuetify.ts
@@ -0,0 +1,26 @@
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/styles';
+
+export default defineNuxtPlugin((app) => {
+  const vuetify = createVuetify({
+    ssr: true,
+    components,
+    directives,
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
+    theme: {
+      defaultTheme: 'light',
+    },
+  });
+
+  app.vueApp.use(vuetify);
+});
