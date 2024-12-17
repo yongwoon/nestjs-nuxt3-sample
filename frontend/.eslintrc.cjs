22745b4ea319270c649d6f0857f7ff19865f3159
yongwoon kim <yonyon.japan@gmail.com>
2024-12-17 13:43:12 +0900

    Setup eslint

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
