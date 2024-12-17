c14fcecb38d894f5a7d652a3c9be70e0f1cc9542
yongwoon kim <yonyon.japan@gmail.com>
2024-12-17 13:53:32 +0900

    version dependency 対応

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,6 +4,12 @@ module.exports = {
     browser: true,
     node: true,
   },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2023,
+    sourceType: 'module',
+  },
   extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
   rules: {
     'vue/multi-word-component-names': 'off',
